b15a12bd689d1743b0f21ecb9e18569beeb4f646
zxcv5608 <asdfghjkl123620@gmail.com>
2020-11-18 14:10:51 +0800

    the blog every post update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-// import Link from 'next/link'
-import { getSortedPostsData } from '../lib/post'
+import Link from 'next/link'
+import Date from '../components/date'
+import { getSortedPostsData } from '../lib/posts'
 
 
 //return allPostData在props物件裡面,blog posts會被傳遞至Home component
@@ -22,13 +23,18 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Blog</h2>
           <ul className={utilStyles.list}>
-            {allPostsData.map(({id, data, title }) => (
+            {allPostsData.map(({id, date, title }) => (
               <li  className={utilStyles.listItem} key={id}>
-               {title}
+               <Link href={`/posts/${id}`}>
+                 <a>{title}</a>
+               </Link>             
               <br />
               {id}
               <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
+              
               </li>
             ))}
 
@@ -40,7 +46,10 @@ export default function Home({ allPostsData }) {
 
 
 export async function getStaticProps() {
+  //從檔案系統,api,db取得外部資料
   const allPostsData = getSortedPostsData()
+
+  //props的值會傳遞到Home component
   return {
     props: {
       allPostsData
